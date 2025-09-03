927cc8cc8d7b4572503f930cd6b7416553191f83
josephcampos-gap <jcamposc@growthaccelerationpartners.com>
2025-09-02 18:13:12 -0600

    Enhance utility functions for caching and logging responses

--- bruno/SIS/v4/EducationOrganization/Calendars/calendars-utils.js
+++ bruno/SIS/v4/EducationOrganization/Calendars/calendars-utils.js
@@ -1,13 +1,12 @@
 // Cache the calendar response values in temporary variables
-function cacheCalendarResponse(bru, response) {
-  if (!!response && response.length === 1) {
+function cacheStoreCalendarResponse(bru, response) {
     const {
       id,
       calendarCode,
       schoolYearTypeReference: schoolYearType,
       gradeLevels,
       calendarTypeDescriptor
-    } = response[0];
+    } = response;
   
     const typeDescriptor = calendarTypeDescriptor.split('#').pop();
     const gradeLevelDescriptors = gradeLevels.map(gl => gl.gradeLevelDescriptor.split('#').pop()).join(', ');
@@ -19,17 +18,51 @@ function cacheCalendarResponse(bru, response) {
     bru.setEnvVar('tempCalendarTypeDescriptor', typeDescriptor);
   
     console.log('Calendar data was fetched correctly.');
-  } else {
-    console.warn('Calendar data not found or multiple records returned, please check the input "Params".');
+}
+
+function cacheWipeCalendarResponse(bru) {
+  bru.setEnvVar('tempCalendarUniqueId', null);
+  bru.setEnvVar('tempCalendarCode', null);
+  bru.setEnvVar('tempCalendarSchoolYear', null);
+  bru.setEnvVar('tempCalendarGradeLevels', null);
+  bru.setEnvVar('tempCalendarTypeDescriptor', null);
+
+  console.warn('Calendar data was wiped. Because data not found or multiple records returned, please check the input "Params".');
+}
+
+function logCalendarResponse(scenarioName, response, filterArray = null) {
+  const {
+      calendarCode,
+      schoolReference,
+      schoolYearTypeReference,
+      calendarTypeDescriptor,
+      gradeLevels
+    } = response;
   
-    bru.setEnvVar('tempCalendarUniqueId', null);
-    bru.setEnvVar('tempCalendarCode', null);
-    bru.setEnvVar('tempCalendarSchoolYear', null);
-    bru.setEnvVar('tempCalendarGradeLevels', null);
-    bru.setEnvVar('tempCalendarTypeDescriptor', null);
-  }
+    const responseToLog = {
+      calendarCode,
+      'schoolId': schoolReference.schoolId,
+      'schoolYear': schoolYearTypeReference.schoolYear,
+      'calendarTypeDescriptor': calendarTypeDescriptor.split('#').pop(),
+      'gradeLevels': gradeLevels.map(level => level.gradeLevelDescriptor.split('#').pop())
+    };
+
+    let objectToLog = responseToLog;
+
+    if (filterArray && Array.isArray(filterArray) && filterArray.length > 0) {
+      objectToLog = filterArray.reduce((acc, key) => {
+        if (responseToLog.hasOwnProperty(key)) {
+          acc[key] = responseToLog[key];
+        }
+        return acc;
+      }, {});
+    }
+
+    console.info(scenarioName, JSON.stringify(objectToLog, null, 2));
 }
 
 module.exports = {
-  cacheCalendarResponse
+  cacheStoreCalendarResponse,
+  cacheWipeCalendarResponse,
+  logCalendarResponse
 };
